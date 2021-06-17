7a21c1fd86b01c813c99a3028738c903d3241db8
Bruno Azuara <bruno.azuara@gmail.com>
2021-06-17 15:28:57 +0200

    refactor info command

--- main.rb
+++ main.rb
@@ -5,6 +5,8 @@ require 'yaml'
 require 'json'
 require 'oauth2'
 require "pp"
+require 'unicode_plot'
+require 'chronic'
 
 # Load credentials
 begin
@@ -23,6 +25,52 @@ client_secret = config["api"]["client_secret"]
 
 client = OAuth2::Client.new(client_id, client_secret, site: "https://api.intra.42.fr")
 
+#info command
+def info_command(client, message, bot)
+  begin
+    user = message.text.split[1]
+	pp "Showing #{user}'s info"
+	token = client.client_credentials.get_token
+	answer = token.get("/v2/users/#{user}").parsed
+	coalition = token.get("/v2/users/#{user}/coalitions").parsed[0]
+
+  #  string_plot = token.get("v2/users/#{user}/locations_stats").parsed
+  #  data = {}
+  #  string_plot.each do |date, hours|
+  #    t = Time.parse(hours)
+  #    hours = t.hour + t.min/60 
+  #    data.store(date, hours)
+  #  end
+  #  UnicodePlot.barplot(data: data, title: "hours").render
+
+    if coalition == nil
+		coa_name = "No coalition"
+	else
+		coa_name = coalition['name']
+	end
+
+	if (answer['cursus_users'].last['blackholed_at'] == nil)
+		blk_str = "No Blackhole"
+	else
+      str_date = answer['cursus_users'].last['blackholed_at']
+      date = DateTime.parse(str_date)
+	  blk_str = "#{(date - DateTime.now).to_i} days, "
+      blk_str << date.strftime("at %d/%m/%y")
+    end
+				
+    bot.api.send_message(chat_id: message.chat.id, text:
+                         "Full Name: #{answer['usual_full_name']}\n"\
+                         "Coalition: #{coa_name}\n"\
+                         "Piscine: #{answer['pool_month'].capitalize} #{answer['pool_year']}\n"\
+                         "Evaluation points #{answer['correction_point'].to_s}\n"\
+                         "Blackholed in #{blk_str}\n"\
+                         "${string_plot}")
+    bot.api.send_photo(chat_id: message.chat.id, photo: answer['image_url'])
+	rescue
+		bot.api.send_message(chat_id: message.chat.id, text: "Something went wrong")
+	end
+end
+
 # Telegram bot config
 
 Telegram::Bot::Client.run(bot_token) do |bot|
@@ -30,38 +78,7 @@ Telegram::Bot::Client.run(bot_token) do |bot|
 	  if message.text.include? '/start'
 		  bot.api.send_message(chat_id: message.chat.id, text: "Hello there, #{message.from.first_name}")
 	  elsif message.text.include? '/info'
-		user = message.text.split[1]
-		pp "Showing #{user}'s info"
-		begin
-			token = client.client_credentials.get_token
-			answer = token.get("/v2/users/#{user}").parsed
-			coalition = token.get("/v2/users/#{user}/coalitions").parsed[0]
-
-			if coalition == nil
-				coa_name = "No coalition"
-			else
-				coa_name = coalition['name']
-			end
-
-			if (answer['cursus_users'].last['blackholed_at'] == nil)
-				blk_str = "No Blackhole"
-			else
-				str_date = answer['cursus_users'].last['blackholed_at']
-				date = DateTime.parse(str_date)
-				blk_str = "#{(date - DateTime.now).to_i} days, "
-                blk_str << date.strftime("at %d/%m/%y")
-			end
-				
-			bot.api.send_message(chat_id: message.chat.id, text:
-			"Full Name: #{answer['usual_full_name']}\n"\
-			"Coalition: #{coa_name}\n"\
-			"Piscine: #{answer['pool_month'].capitalize} #{answer['pool_year']}\n"\
-			"Evaluation points #{answer['correction_point'].to_s}\n"\
-			"Blackholed in #{blk_str}")
-			bot.api.send_photo(chat_id: message.chat.id, photo: answer['image_url'])
-		rescue
-			bot.api.send_message(chat_id: message.chat.id, text: "Something went wrong")
-		end
+        info_command(client, message, bot)
 	  elsif message.text.include? '/stop'
 		bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
 	  else
